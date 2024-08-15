569d7254ed8aa525298241be9ac43d92ea5a1f6c
Thiago Moura <waatsaaynew@gmail.com>
2024-08-15 00:34:03 -0300

    modal js

--- scripts.js
+++ scripts.js
@@ -1,63 +1,23 @@
-function alteraClasse(classe) {
-    document.getElementById("cards").setAttribute("class", classe)
-}
-
-function alteraModal(classe, titulo, corpo) {
-    document.getElementById("modal").setAttribute("class", classe)
-    document.getElementById("overlay").setAttribute("class", classe)
+function fechaModal() {
+    // Seleciona o primeiro elemento com a classe 'overlay'
+    let element = document.querySelector(".overlay");
     
-    if(classe == 'on'){
-        document.getElementById("titulo-modal").innerText = titulo
-        document.getElementById("corpo-modal").innerText = corpo
-    }else{
-        document.querySelector(".card-aberto").classList.remove("card-aberto")
+    // Verifica se o elemento foi encontrado
+    if (element) {
+        element.classList.remove("overlay");
+        element.classList.add("out");
     }
 }
 
-function selecionaNaTelaPequena(){
-    selecionado = document.getElementById("menu-cabecalho-tela-pequena").value;
-
-    switch (selecionado) {
-        case "l": alteraClasse("lista"); break;
-        case "gg": alteraClasse("grade-grande"); break;
-        case "gp": alteraClasse("grade-pequena");break;
+function abrirModal(){
+    let element = document.querySelector(".out");
+    if(element){
+        element.classList.remove("out");
+        element.classList.add("overlay")
     }
 }
 
-function addItemLista(){
-    document.getElementById("msg-lista-vazia").style.display = "none"
-    var li = document.createElement("li")
-    
-    var input = document.createElement("input")
-    input.setAttribute("type", "text")
-    input.setAttribute("placeholder", "Informe o título...")
-    li.appendChild(input)
+function adicionarAnotacao(){
 
-    var textArea = document.createElement("textarea")
-    textArea.setAttribute("placeholder", "Informe o texto...")
-    li.appendChild(textArea)
-    
-    var button = document.createElement("button")
-    button.innerText = "Ver"
-    button.disabled = true
-    li.appendChild(button)
-
-    button.addEventListener("click", function(){
-        li.classList.add("card-aberto")
-        alteraModal("on", input.value, textArea.value)
-    })
-
-    function toggleBotao(){
-        console.log(input.value)
-        if(input.value.trim().length > 0 && textArea.value.trim().length > 0 ){
-            button.disabled = false
-        }else{
-            button.disabled = true
-        }
-    }
-
-    input.addEventListener("keyup", toggleBotao)
-    textArea.addEventListener("keyup", toggleBotao)
-
-    document.getElementById("cards").appendChild(li)
+    fechaModal();
 }
